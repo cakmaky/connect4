d566a7e5d713a126a59cb310ff0a613309166e26
cakmaky <yalcincakmak009@gmail.com>
2017-10-26 13:10:21 -0700

    Initial Commit

---
+++ connect4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  connect4
+//
+//  Created by YC on 10/26/17.
+//  Copyright © 2017 Bright Beam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
